36ca5c563c5f787cf6ead24708d94fd1d95184c4
Oksana <oksastar89@mail.ru>
2021-02-01 23:07:27 +0300

    homework8

--- lesson8/task6.py
+++ lesson8/task6.py
@@ -6,7 +6,7 @@
 import numbers
 
 
-def check_valid_str(s):
+def check_valid_str(s: str):
     if s is None or len(s) == 0:
         raise ValueError('Invalid string')
     return s
@@ -18,14 +18,28 @@ def check_price(price):
     return price
 
 
-def check_count(count):
+def check_count(count) -> int:
     if not isinstance(count, numbers.Number) or int(count) <= 0:
         raise ValueError(f'Invalid count [value={count}]')
     return count
 
 
+class OfficeEquipment:
+    def __init__(self, name: str, price: float, make: str, model: str):
+        self.name = check_valid_str(name)
+        self.price = check_price(price)
+        self.make = check_valid_str(make)
+        self.model = check_valid_str(model)
+
+    def __repr__(self):
+        return f'{self.name}[price={self.price}, make={self.make}, model={self.model}]'
+
+    def unique_name(self):
+        return f'{self.name}:{self.make}:{self.model}'
+
+
 class Item:
-    def __init__(self, office_eq, count):
+    def __init__(self, office_eq: OfficeEquipment, count: int):
         self.office_eq = office_eq
         self.count = check_count(count)
 
@@ -39,10 +53,14 @@ class Storage:
         self._dict = {}
         pass
 
-    def accept(self, item):
-        self._dict[item.office_eq.unique_name()] = item
+    def accept(self, item: Item):
+        prev: Item = self._dict.get(item.office_eq.unique_name())
+        if prev is None:
+            self._dict[item.office_eq.unique_name()] = item
+        else:
+            self._dict[item.office_eq.unique_name()] = Item(item.office_eq, prev.count + item.count)
 
-    def move_to(self, req_item, storage):
+    def move_to(self, req_item: Item, storage):
         check_count(req_item.count)
         rem_item = self._dict.get(req_item.office_eq.unique_name())
         if rem_item is None:
@@ -51,34 +69,20 @@ class Storage:
             raise ValueError(f'Requested items count more than remainder in storage '
                              f'[requested={req_item.count}, remainder={rem_item.count}]')
         storage.accept(req_item)
-        self._dict[rem_item.office_eq.name] = Item(rem_item.office_eq, rem_item.count - req_item.count)
+        self._dict[rem_item.office_eq.unique_name()] = Item(rem_item.office_eq, rem_item.count - req_item.count)
 
     def print_all(self):
         print(f'Items in storage: {self._dict}')
 
 
 class Department(Storage):
-    def __init__(self, name):
+    def __init__(self, name: str):
         Storage.__init__(self)
         self.name = check_valid_str(name)
         pass
 
     def print_all(self):
         print(f'Items in storage: [department={self.name}, items={self._dict}]')
-
-
-class OfficeEquipment:
-    def __init__(self, name, price, make, model):
-        self.name = check_valid_str(name)
-        self.price = check_price(price)
-        self.make = check_valid_str(make)
-        self.model = check_valid_str(model)
-
-    def __repr__(self):
-        return f'{self.name}[price={self.price}, make={self.make}, model={self.model}]'
-
-    def unique_name(self):
-        return f'{self.name}:{self.make}:{self.model}'
 
 
 class Xerox(OfficeEquipment):
@@ -108,6 +112,7 @@ scanner_2 = Scanner(17000, 'Canon', 'PIXMA')
 xerox_2 = Xerox(25000, 'Pantum', 'M6607')
 
 main_storage.accept(Item(printer_1, 3))
+main_storage.accept(Item(printer_1, 5))
 main_storage.accept(Item(scanner_2, 5))
 main_storage.accept(Item(scanner_1, 6))
 main_storage.accept(Item(printer_2, 6))
@@ -119,5 +124,6 @@ main_storage.move_to(Item(printer_1, 2), dep1)
 main_storage.move_to(Item(scanner_1, 1), dep1)
 main_storage.move_to(Item(xerox_1, 2), dep2)
 
+main_storage.print_all()
 dep1.print_all()
 dep2.print_all()
